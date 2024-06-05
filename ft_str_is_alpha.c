fe6a8b9c27526ecb48eab7106a74a1c9dd27a3c9
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-05 15:09:51 +0200

    EX02

--- ft_str_is_alpha.c
+++ ft_str_is_alpha.c
@@ -1,39 +1,31 @@
-
-
-int ft_str_is_alpha(char *str)
+int	ft_str_is_alpha(char *str)
 {
-    // Verificar si el string es vacío
-    if (*str == ' ')
-        return 1;
-
-    while (*str)
-    {
-        if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
-        {
-            str++;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    return 1;
+	if (*str == '\0')
+		return (1);
+	while (*str)
+	{
+		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+		{
+			str ++ ;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
 #include <stdio.h>
-int main()
-{
-    char *str = "ldkns dfgf  onih";
+int main() {
+
+    char *str="sdhdsfhHDFGH";
     int resultado = ft_str_is_alpha(str);
 
-    if (resultado == 0)
-    {
-        printf("La función devolvió 0: otros tipos de caracteres o caracteres con espacios\n");
-    }
-    else if (resultado == 1)
-    {
-        printf("La función devolvió 1: alfanuméricos o espacios en blanco\n");
-    }
 
+    if (resultado == 0) {
+        printf("La función devolvió 0: otro tipo de caracteres\n");
+    } else if (resultado == 1) {
+        printf("La función devolvió 1: alfanumericos o espacios en blanco\n");
+    }
     return 0;
 }
