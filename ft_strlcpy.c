80707ba277f60d86ee4c101596b6b0a6a106a3cc
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-08 16:22:00 +0200

    ex10

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -2,6 +2,7 @@ int	ft_strlen(char *str)
 
 {
     int i;
+    
     i= 0;
     while (str[i] != '\0')
     {
@@ -13,20 +14,20 @@ int	ft_strlen(char *str)
 unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 
 { 
-    unsigned int src_len =ft_strlen(src);
+    unsigned int src_len;
     unsigned int i;
-
-    if (size !=0 )
     
+    src_len =ft_strlen(src);
+    
+    if (size != 0 )
     {
-            i=0;
-        
+            i = 0;
             while (i < size -1 && src[i] != '\0')
             {
-                dest[i] =src[i];
+                dest[i] = src[i];
                 i ++;
             }
-            dest[i]= '\0';
+            dest[i] = '\0';
     }
     return src_len;
 }
@@ -35,12 +36,18 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 #include <stdio.h>
 int main (void)
 {
-    char origen [] ="hola";
-    char destino[10];
-    unsigned int size = sizeof(destino)/ sizeof(destino[0]);
+    char origen [] ="hola caracola";
+    char destino[20];
+    unsigned int size = sizeof(destino)/ sizeof(destino[0]); /*calcula el tamaño del array destino dividiendo el tamaño total del array por el tamaño de un solo elemento del array. Esto nos da el número de elementos en el array destino*/
     
     ft_strlcpy(destino, origen, size);
-    printf("%s\n", destino);
+    
+   
+    printf("string origen: %s\n", destino);
+    printf("string destino: %s\n", origen);
+    printf("size :%i\n", size);
+    printf("tamaño de origen:%i\n", sizeof(origen));
+    printf("tamaño de destino:%i\n", sizeof(destino));
     
     return 0;
 }
