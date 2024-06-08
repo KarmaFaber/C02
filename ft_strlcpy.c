799ab28754525e3e0a838c148dcba9525c7ad52f
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-08 17:06:37 +0200

    ex10

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,32 +1,32 @@
 int	ft_strlen(char *str)
 {
-    int i;
-    
-    i= 0;
-    while (str[i] != '\0')
-    {
-        i ++ ;
-    }
-    return (i);
+	int	i;
+
+	i= 0;
+	while (str[i] != '\0')
+	{
+		i ++ ;
+	}
+	return (i);
 }
 
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
-{ 
-    unsigned int src_len;
-    unsigned int i;
-    
-    src_len =ft_strlen(src);
-    if (size != 0 )
-    {
-            i = 0;
-            while (i < size -1 && src[i] != '\0')
-            {
-                dest[i] = src[i];
-                i ++;
-            }
-            dest[i] = '\0';
-    }
-    return src_len;
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	src_len;
+	unsigned int	i;
+
+	src_len = ft_strlen(src);
+	if (size != 0)
+	{
+		i = 0;
+		while (i < size -1 && src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i ++ ;
+		}
+		dest[i] = '\0';
+	}
+	return (src_len);
 }
 
 
