0a1cf9b78afd2419f7d57c5b1f03d2e0d55934b1
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-08 15:40:54 +0200

    ex08

--- ft_strlowcase.c
+++ ft_strlowcase.c
@@ -5,7 +5,7 @@ char *ft_strlowcase(char *str)
     i = 0;
     while (str [i] != '\0')
     {
-        if (str[i] >= 'A' && str i] <= 'Z')
+        if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] = str[i] + 32; 
         i ++ ;
     }
