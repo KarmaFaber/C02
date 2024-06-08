51a92da4fe087b2348d53d49a85f4a28999a3806
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-08 17:15:13 +0200

    ex11

--- ft_putstr_non_printable.c
+++ ft_putstr_non_printable.c
@@ -1,18 +1,19 @@
 #include <unistd.h>
 
-void ft_putstr_non_printable(char *str) {
-    int i = 0;
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
 
-    while (str[i] != '\0') {
-        if (str[i] >= 32 && str[i] <= 126) 
-            write(1, &str[i], 1);
-        else 
-            write(1, "\\0", 2);
-            
-        i++;
-    }
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			write (1, &str[i], 1);
+		else
+			write (1, "\\0", 2);
+		i ++ ;
+	}
 }
-
 /*int main(void) {
     ft_putstr_non_printable("Coucou\vtu vas bien ?");
     return 0;
