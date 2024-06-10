ec532be2f9c78e3caa59cfcf7bf1b02c23e09997
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-10 21:39:49 +0200

    ex11_v2

--- ft_putstr_non_printable.c
+++ ft_putstr_non_printable.c
@@ -1,5 +1,20 @@
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_puthex(unsigned char c)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	ft_putchar('\\');
+	ft_putchar(hex[c / 16]);
+	ft_putchar(hex[c % 16]);
+}
+
 void	ft_putstr_non_printable(char *str)
 {
 	int	i;
@@ -8,13 +23,14 @@ void	ft_putstr_non_printable(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 32 && str[i] <= 126)
-			write (1, &str[i], 1);
+			ft_putchar(str[i]);
 		else
-			write (1, "\\0", 2);
-		i ++ ;
+			ft_puthex((unsigned char)str[i]);
+		i++;
 	}
 }
 /*int main(void) {
-    ft_putstr_non_printable("Coucou\vtu vas bien ?");
+    ft_putstr_non_printable("Coucou\ntu vas bien ?");
     return 0;
 }*/
+
