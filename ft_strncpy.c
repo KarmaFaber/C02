db031df976af8e320940dbddd4a5f0e87699c759
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-11 15:10:21 +0200

    ex01_v3

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,14 +6,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
-		i ++ ;
+		i++;
 	}
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
 /*#include <stdio.h>
